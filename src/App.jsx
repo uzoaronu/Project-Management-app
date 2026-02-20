5e9f7d9699f7a563595974cc0fd828ccee8dd4d8
Uzo Aronu <uzo.code123@gmail.com>
2026-02-20 06:06:01 +0100

    Implemented Projects sidebar, new project and No Projectselected components

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import NoProjectSelected from './components/NoProjectSelected';
+import ProjectsSidebar from './components/ProjectsSidebar';
+
+function App() {
+  return (
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectsSidebar />
+      <NoProjectSelected />
+    </main>
+  );
+}
+
+export default App;
