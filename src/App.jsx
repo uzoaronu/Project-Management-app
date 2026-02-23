38beb7eea33487178c3f8d20ef4b281a03eb0bcc
Uzo Aronu <uzo.code123@gmail.com>
2026-02-23 05:19:02 +0100

    Implemented UseRefs and ForwardRefs for collecting user inputs

--- src/App.jsx
+++ src/App.jsx
@@ -16,10 +16,19 @@ function App() {
     });
   }
 
+  function handleAddProject(projectData) {
+    setProjectsState((prevState) => {
+      const newProject = { ...projectData, id: Math.random().toString() };
+      return { ...prevState, projects: [...prevState.projects, newProject] };
+    });
+  }
+
+  console.log(projectsState);
+
   let content;
 
   if (projectsState.selectedProjectId === null) {
-    content = <NewProject />;
+    content = <NewProject onAdd={handleAddProject} />;
   } else if (projectsState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartProject} />;
   }
