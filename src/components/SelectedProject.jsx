9bcfd35a66e7814ec01cbca402bb0832e431d7aa
Uzo Aronu <uzo.code123@gmail.com>
2026-03-12 21:05:52 +0100

    implemented NewTask component

--- src/components/SelectedProject.jsx
+++ src/components/SelectedProject.jsx
@@ -1,4 +1,12 @@
-export default function SelectedProject({ project }) {
+import Tasks from './Tasks';
+
+export default function SelectedProject({
+  project,
+  onDelete,
+  onAddTask,
+  onDeleteTask,
+  tasks,
+}) {
   const formattedDate = new Date(project.dueDate).toLocaleDateString('en-US', {
     day: 'numeric',
     month: 'long',
@@ -11,7 +19,10 @@ export default function SelectedProject({ project }) {
           <h1 className="text-3xl font-bold text-stone-600 mb-2">
             {project.title}
           </h1>
-          <button className="text-stone-600 hover:text-stone-950">
+          <button
+            className="text-stone-600 hover:text-stone-950"
+            onClick={onDelete}
+          >
             Delete
           </button>
         </div>
@@ -20,7 +31,7 @@ export default function SelectedProject({ project }) {
           {project.description}
         </p>
       </header>
-      TASKS
+      <Tasks onAdd={onAddTask} onDelete={onDeleteTask} tasks={tasks} />
     </div>
   );
 }
